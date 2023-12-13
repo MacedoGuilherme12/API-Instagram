1234bb1f4bd0d376079b8c1258baffd3ecf08c04
Guilherme <guilhermecedoanastacio@gmail.com>
2023-12-13 19:47:40 -0300

    Validação Email

--- src/apps/controllers/UsersControllers.js
+++ src/apps/controllers/UsersControllers.js
@@ -3,8 +3,16 @@ const Users = require('../models/Users')
 class UsersControllers {
     async create(req, res) {
         console.log(req.body)
+        const verifyUser = await Users.findOne({
+            where : {
+                email : req.body.email
+            }
+        })
+        if(verifyUser){
+             return res.status(400).json({ message : "User already exists!  " })
+        }
         const user = await Users.create(req.body)
-        res.send({ message : "Teste"})
+        res.send({ user })
     }
 }
 

d438bf7cc2586d24f3484e4d46bd4a5b0c699375
Guilherme <guilhermecedoanastacio@gmail.com>
2023-12-14 09:48:11 -0300

    Schema Validator

--- src/apps/controllers/UsersControllers.js
+++ src/apps/controllers/UsersControllers.js
@@ -3,8 +3,19 @@ const Users = require('../models/Users')
 class UsersControllers {
     async create(req, res) {
         console.log(req.body)
+        const verifyUser = Users.findOne({
+            where : {
+                email : req.body.email
+            }
+        })
+        if(verifyUser){
+            return res.status(400).json({ message : "User already exits! "})
+        }
+        if(!user){
+            return res.status(400).json({ message : "Failed to create user "})
+        }
         const user = await Users.create(req.body)
-        res.send({ message : "Teste"})
+        return res.send({ message: "Teste" })
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 const {Router} = require('express')
 const UsersControllers = require('./apps/controllers/UsersControllers')
-
+const schemaValidator = require('./apps/middlewares/schemaValidator')
+const userSchema = require('./apps/schema/create.user.schema.json')
 const routes = new Router()
 
 
-routes.post('/user', UsersControllers.create )
+routes.post('/user', schemaValidator(userSchema), UsersControllers.create )
 
 
 routes.get('/health', (req, res) =>{

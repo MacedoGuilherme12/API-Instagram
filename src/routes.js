bbf57f967cb7400f367076ce7698f3e3b32d7b9d
Guilherme <guilhermecedoanastacio@gmail.com>
2023-12-15 11:14:46 -0300

    Auth Schema

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,12 @@ const UsersControllers = require('./apps/controllers/UsersControllers')
 const schemaValidator = require('./apps/middlewares/schemaValidator')
 const userSchema = require('./apps/schema/create.user.schema.json')
 const AuthenticationController = require('./apps/controllers/AuthenticationController')
+const authSchema = require('./apps/schema/auth.schema.json')
 const routes = new Router()
 
 
 routes.post('/user', schemaValidator(userSchema), UsersControllers.create )
-routes.post('/auth', AuthenticationController.Authenticate)
+routes.post('/auth', schemaValidator(authSchema), AuthenticationController.Authenticate)
 
 routes.get('/health', (req, res) =>{
     return res.send({messsage: "Connected with sucess"})

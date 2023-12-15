50949156fd556563955c896cfbeb9497c54c0d94
Guilherme <guilhermecedoanastacio@gmail.com>
2023-12-15 10:25:13 -0300

    Password authenticator

--- src/apps/controllers/AuthenticationController.js
+++ src/apps/controllers/AuthenticationController.js
@@ -6,25 +6,27 @@ class AuthenticationController{
     async Authenticate(req,res){
         const { email, user_name, password } = req.body
 
-        let whereClause = {}
+        let whereClause = { }
         if(email){
-            whereClause = {email}
+            whereClause.email = email
         } else if(user_name){
-            whereClause = {user_name}
+            whereClause.user_name = user_name
         } else{
             return res.status(401).json({message : "We need a email or password"})
         }
 
 
-        console.log(whereClause)
-        const verifyUser = await Users.findOne({
+        const user = await Users.findOne({
             where:  whereClause
         })
 
-        if(!verifyUser){
+        if(!user){
             return res.status(401).json({ message : 'User not Found'})
         }
-        return res.status(200).json({ user : verifyUser})
+        if(!user.checkPassword(password)){
+            return res.status(401).json({ message : 'Password does not match!!'})
+        }
+        return res.status(200).json({ user })
     }
 }
 
